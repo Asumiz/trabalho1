a756a9f604ab45a3e660c8da823d9d4e8e772d42
2830972513009 <2830972513009>
2026-09-18 20:57:24 -0300

    o meu

---
+++ database.sql
@@ -0,0 +1,14 @@
+insert into usuario (usuario, senha)
+values ('gustavo', '123')
+
+select * from usuario
+create database desevolvimento_software
+
+nomecreate table usuarios (
+	id int auto_increment primary key,
+    nome VARCHAR(100) not null,
+    usuario varchar(50) not null unique,
+    senha varchar(255) not null 
+);
+
+select * from usuarios
